e05b37c0af107c41e74cb97aee70b26197009fba
1strategic.learner <1strategic.learner@gmail.com>
2016-12-22 16:49:48 -0800

    More early Model tweaks

---
+++ MVC02/AppConstants/AppConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MVC02.AppConstants
+    {
+    public class AppConstants
+        {
+        public static DateTime defaultEndDate()
+            {
+            return Convert.ToDateTime("2999-01-01");
+            }
+
+        }
+    }
