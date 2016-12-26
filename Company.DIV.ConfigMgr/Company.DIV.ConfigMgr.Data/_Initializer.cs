8a01975432adb3269d3049a8f377d66c0d098413
1strategic.learner <1strategic.learner@gmail.com>
2016-12-25 19:11:34 -0800

    Changing .Data project to Application build type cleaning up some sln+project  configuration settings

---
+++ Company.DIV.ConfigMgr/Company.DIV.ConfigMgr.Data/_Initializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.DIV.ConfigMgr.Data
+    {
+    internal class ConfigMgrDropCreateAlwaysInitializer :
+        DropCreateDatabaseAlways<ConfigMgrContext>
+    //DropCreateDatabaseIfModelChanges<ConfigMgrContext>
+        {
+
+        public ConfigMgrDropCreateAlwaysInitializer()
+            {
+
+            }
+
+        }
+    }
---
+++ Company.DIV.ConfigMgr/Company.DIV.ConfigMgr.Data/__Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.DIV.ConfigMgr.Data
+    {
+    public static class Program
+        {
+        public static void Main()
+            {
+#if DEBUG
+            Database.SetInitializer<ConfigMgrContext>(new ConfigMgrDropCreateAlwaysInitializer());
+            //Database.SetInitializer(new ConfigMgrInitializer());
+#else
+            Database.SetInitializer<ConfigMgrContext>(new ConfigMgrDropCreateAlwaysInitialize());
+#endif
+
+            }
+        }
+    }
